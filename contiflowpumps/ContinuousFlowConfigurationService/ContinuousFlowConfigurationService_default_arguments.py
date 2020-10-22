3c44fbb5a280fa0d9a91e3a213def1e061f968e8
Florian Meinicke <florian.meinicke@cetoni.de>
2020-10-22 09:22:08 +0200

    Add generated prototype code for contiflow features
    Change-Id: I1cf54bba585d25c138c3e652a5b2434f6ebd9ef3

---
+++ contiflowpumps/ContinuousFlowConfigurationService/ContinuousFlowConfigurationService_default_arguments.py
@@ -0,0 +1,72 @@
+# This file contains default values that are used for the implementations to supply them with 
+#   working, albeit mostly useless arguments.
+#   You can also use this file as an example to create your custom responses. Feel free to remove
+#   Once you have replaced every occurrence of the defaults with more reasonable values.
+#   Or you continue using this file, supplying good defaults..
+
+# import the required packages
+import sila2lib.framework.SiLAFramework_pb2 as silaFW_pb2
+import sila2lib.framework.SiLABinaryTransfer_pb2 as silaBinary_pb2
+from .gRPC import ContinuousFlowConfigurationService_pb2 as pb2
+
+# initialise the default dictionary so we can add keys. 
+#   We need to do this separately/add keys separately, so we can access keys already defined e.g.
+#   for the use in data type identifiers
+default_dict = dict()
+
+
+default_dict['SetSwitchingMode_Parameters'] = {
+    'SwtichingMode': silaFW_pb2.String(value='default string')
+}
+
+default_dict['SetSwitchingMode_Responses'] = {
+    
+}
+
+default_dict['SetRefillFlowRate_Parameters'] = {
+    'RefillFlowRate': silaFW_pb2.Real(value=0.0)
+}
+
+default_dict['SetRefillFlowRate_Responses'] = {
+    
+}
+
+default_dict['SetCrossFlowDuration_Parameters'] = {
+    'CrossFlowDuration': silaFW_pb2.Real(value=0.0)
+}
+
+default_dict['SetCrossFlowDuration_Responses'] = {
+    
+}
+
+default_dict['SetOverlapDuration_Parameters'] = {
+    'OverlapDuration': silaFW_pb2.Real(value=0.0)
+}
+
+default_dict['SetOverlapDuration_Responses'] = {
+    
+}
+
+default_dict['Subscribe_SwitchingMode_Responses'] = {
+    'SwitchingMode': silaFW_pb2.String(value='default string')
+}
+
+default_dict['Subscribe_MaxRefillFlowRate_Responses'] = {
+    'MaxRefillFlowRate': silaFW_pb2.Real(value=0.0)
+}
+
+default_dict['Subscribe_RefillFlowRate_Responses'] = {
+    'RefillFlowRate': silaFW_pb2.Real(value=0.0)
+}
+
+default_dict['Subscribe_MinFlowRate_Responses'] = {
+    'MinFlowRate': silaFW_pb2.Real(value=0.0)
+}
+
+default_dict['Subscribe_CrossFlowDuration_Responses'] = {
+    'CrossFlowDuration': silaFW_pb2.Real(value=0.0)
+}
+
+default_dict['Subscribe_OverlapDuration_Responses'] = {
+    'OverlapDuration': silaFW_pb2.Real(value=0.0)
+}
