b47b670c07a7e6169200bef94bcc1ec6290881dd
Florian Meinicke <florian.meinicke@cetoni.de>
2022-06-23 11:25:54 +0200

    Change path of config files
    Config files are now stored into an additional subdirectory that is
    named the same as the used device configuration (or 'empty' if no device
    configuration is used). That way devices with the same name from
    different device configs get unique IDs etc.

--- sila_cetoni/pumps/syringepumps/sila/syringepump_service/feature_implementations/pumpdrivecontrolservice_impl.py
+++ sila_cetoni/pumps/syringepumps/sila/syringepump_service/feature_implementations/pumpdrivecontrolservice_impl.py
@@ -57,7 +57,7 @@ class PumpDriveControlServiceImpl(PumpDriveControlServiceBase):
         self.__pump = pump
         self.__is_initializing = False
         self.__system = ApplicationSystem()
-        self.__config = Config(self.__pump.get_pump_name())
+        self.__config = Config(self.__pump.get_pump_name(), self.__system.device_config.name)
         self.__stop_event = Event()
 
         if not self.__pump.is_position_sensing_initialized():
